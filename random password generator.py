71a8ecabd1bec719496e6e69990e59651ef71de6
paankaka <79742205+paankaka@users.noreply.github.com>
2021-03-10 13:07:08 +0530

    Add files via upload

---
+++ random password generator.py
@@ -0,0 +1,15 @@
+import random
+def password(length=8):
+    l = ['@','#','$','%','&']
+    upper = chr(random.randint(65,90))
+    lower = chr(random.randint(97,122))
+    special = random.choice(l)
+    digit = random.randint(10000,99999)
+    generate = upper + lower + special + str(digit)
+    l = random.sample(generate)
+    generate = ("").join(l)
+    return generate 
+
+result = password(5)
+print(result)
+    
